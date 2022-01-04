b93058437554a03ba82bee3a6b55792397841062
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-01-04 09:52:26 +0200

    v1.3

--- customize.sh
+++ customize.sh
@@ -9,10 +9,10 @@ unzip -o "$ZIPFILE" system.prop -d $MODPATH >&2
 # Bootloader string.
 BL=$(getprop ro.boot.bootloader);
 # Device is first 5 characters of bootloader string.
-DEVICE=${BL:0:$((${#BL} - 8))};
+DEVICE=${BL:0:5};
 # Zero last character of device string.
 DEVICEOFF=${DEVICE%?}0;
 # Replace BASIC string with DEVICEOFF string.
-sed -i "s/BASIC/${DEVICEOFF}/g" $MODPATH/system.prop;
+sed -i "s/BASIC/SM\-${DEVICEOFF}/g" $MODPATH/system.prop;
 
 set_perm_recursive "$MODPATH" 0 0 0755 0644
