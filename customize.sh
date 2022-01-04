76ce21a26f6a6276757b7b403553a9cd8d870838
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-01-04 09:57:04 +0200

    v3.0

--- customize.sh
+++ customize.sh
@@ -2,17 +2,37 @@ SKIPUNZIP=1
 
 # Extract files
 ui_print "- Extracting module files"
-
 unzip -o "$ZIPFILE" module.prop -d $MODPATH >&2
 
+# paths
+patched_keystore="$MODPATH/system/bin/keystore"
+original_keystore="/bin/keystore"
+replace_path="$MODPATH/system/bin"
 # os string.
 os=$(getprop ro.build.version.release)
 major=${os%%.*}
 
-# Extract keystore executables and libraries
-unzip -o "$ZIPFILE" "system_$major/*" -d "$MODPATH" >&2;
-mv $MODPATH/system_$major $MODPATH/system
+ui_print "- Checking Android version..."
+if [[ "$major" == "11" ]]; then
+  pre="790074696d657374616d7000636f6d2e"
+  post="790000696d657374616d7000636f6d2e"
+elif [[ "$major" == "10" ]]; then
+  pre="5d0074696d657374616d7000636f6d2e"
+  post="5d0000696d657374616d7000636f6d2e"
+else
+  ui_print "- Unsupported Android version. Aborting..."
+  abort
+fi
 
-# Set executable permissions
-set_perm_recursive "$MODPATH" 0 0 0755 0644
-set_perm_recursive $MODPATH/system/bin 0 0 0755 0755
+ui_print "- Found Android $os, Start patching..."
+ls $replace_path
+mkdir -p $replace_path
+cp -f -p $original_keystore $replace_path
+set_perm_recursive $replace_path 0 0 0755 0755
+if `/data/adb/magisk/magiskboot hexpatch $patched_keystore $pre $post` ; then
+  ui_print "- Successfully patched!"
+else
+  ui_print "- Not found hex pattern in keystore, Aborting..."
+  rm -rf $MODPATH
+  abort
+fi
