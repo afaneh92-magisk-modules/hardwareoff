5f8895781dab7fa33b7d3427f8da189dac6e3159
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-01-04 09:45:08 +0200

    v1.0

---
+++ customize.sh
@@ -0,0 +1,18 @@
+SKIPUNZIP=1
+
+# extract files
+ui_print "- Extracting module files"
+
+unzip -o "$ZIPFILE" module.prop -d $MODPATH >&2
+unzip -o "$ZIPFILE" system.prop -d $MODPATH >&2
+
+# Bootloader string.
+BL=$(getprop ro.boot.bootloader);
+# Device is first 5 characters of bootloader string.
+DEVICE=${BL:0:$((${#BL} - 8))};
+# Zero last character of device string.
+DEVICEOFF=${DEVICE%?}0;
+# Replace BASIC string with DEVICEOFF string.
+sed -i "s/BASIC/${DEVICEOFF}/g" $MODPATH/system.prop;
+
+set_perm_recursive "$MODPATH" 0 0 0755 0644
