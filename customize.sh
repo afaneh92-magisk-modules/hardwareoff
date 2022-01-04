f452b90946fecae1cb824fb72f0c3bea5c7b7bd7
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-01-04 09:54:07 +0200

    v1.6

--- customize.sh
+++ customize.sh
@@ -4,20 +4,6 @@ SKIPUNZIP=1
 ui_print "- Extracting module files"
 
 unzip -o "$ZIPFILE" module.prop -d $MODPATH >&2
-unzip -o "$ZIPFILE" system.prop -d $MODPATH >&2
-unzip -o "$ZIPFILE" post-fs-data.sh -d $MODPATH >&2
-
-# Bootloader string.
-BL=$(getprop ro.boot.bootloader);
-# Device is first 5 characters of bootloader string.
-DEVICE=${BL:0:5};
-DEVICEVARIANT=${BL:4:1};
-# Zero last character of device string.
-DEVICEOFF=${DEVICE%?}0;
-# U last character of device string for HK devices.
-[ "$DEVICEVARIANT" = "0" ] && DEVICEOFF=${DEVICE%?}U;
-# Replace BASIC string with DEVICEOFF string.
-sed -i "s/BASIC/SM\-${DEVICEOFF}/g" $MODPATH/system.prop;
 
 # os string.
 os=$(getprop ro.build.version.release)
